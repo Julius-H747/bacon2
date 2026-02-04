5ad707f2108947ce96b38f17843696696395e91c
Julius-H747 <julius.hold@gmail.com>
2026-02-04 20:17:09 +1000

    stuff

--- bacon-2/character_body_2d.gd
+++ bacon-2/character_body_2d.gd
@@ -1,74 +1,79 @@
 extends CharacterBody2D
 
-@export var speed: float = 200.0
+@export var speed := 250.0
+@export var jump_force := 450.0
+@export var gravity := 1200.0
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-var last_direction := Vector2.DOWN
-var was_moving := false
+var facing := "right"
+var was_running := false
 
 func _physics_process(delta):
-	var input_vector = Vector2(
-		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	).normalized()
+	# Gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
-	velocity = input_vector * speed
+	# Jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -jump_force
+		play_jump()
+
+	# Horizontal movement
+	var direction := Input.get_axis("move_left", "move_right")
+
+	if direction != 0:
+		velocity.x = direction * speed
+		facing = "right" if direction > 0 else "left"
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
 	move_and_slide()
+	update_animation(direction)
 
-	update_animation(input_vector)
+func update_animation(direction: float):
+	# In air
+	if not is_on_floor():
+		if velocity.y < 0:
+			play_jump()
+		else:
+			play_fall()
+		return
 
-func update_animation(input_vector: Vector2):
-	if input_vector != Vector2.ZERO:
-		last_direction = input_vector
-		was_moving = true
-		play_run_animation(input_vector)
+	# Running
+	if direction != 0:
+		was_running = true
+		play_run()
+		return
+
+	# Run → Idle
+	if was_running:
+		play_run_to_idle()
+		was_running = false
+		return
+
+	# Idle
+	play_idle()
+
+func play_idle():
+	sprite.play("idle_" + facing)
+
+func play_run():
+	sprite.play("run_" + facing)
+
+func play_run_to_idle():
+	var anim := "run_to_idle_" + facing
+	if sprite.sprite_frames.has_animation(anim):
+		sprite.play(anim)
 	else:
-		if was_moving:
-			play_run_to_idle_animation()
-			was_moving = false
-		else:
-			play_idle_animation()
+		play_idle()
 
-func play_run_animation(dir: Vector2):
-	if abs(dir.x) > abs(dir.y):
-		if dir.x > 0:
-			sprite.play("run_right")
-		else:
-			sprite.play("run_left")
+func play_jump():
+	sprite.play("jump_" + facing)
+
+func play_fall():
+	var anim := "fall_" + facing
+	if sprite.sprite_frames.has_animation(anim):
+		sprite.play(anim)
 	else:
-		if dir.y > 0:
-			sprite.play("run_down")
-		else:
-			sprite.play("run_up")
-
-func play_idle_animation():
-	if abs(last_direction.x) > abs(last_direction.y):
-		if last_direction.x > 0:
-			sprite.play("idle_right")
-		else:
-			sprite.play("idle_left")
-	else:
-		if last_direction.y > 0:
-			sprite.play("idle_down")
-		else:
-			sprite.play("idle_up")
-
-func play_run_to_idle_animation():
-	var anim_name := ""
-
-	if abs(last_direction.x) > abs(last_direction.y):
-		if last_direction.x > 0:
-			anim_name = "run_to_idle_right"
-		else:
-			anim_name = "run_to_idle_left"
-	else:
-		if last_direction.y > 0:
-			anim_name = "run_to_idle_down"
-		else:
-			anim_name = "run_to_idle_up"
-
-	if sprite.sprite_frames.has_animation(anim_name):
-		sprite.play(anim_name)
-	else:
-		play_idle_animation()
+		play_jump()
